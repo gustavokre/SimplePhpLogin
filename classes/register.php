28509884b4a04b53b3731f748ae67e2992d25c6b
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-03 20:35:40 -0300

    pequenas adições

--- classes/register.php
+++ classes/register.php
@@ -1,6 +1,9 @@
 <?php
 
     class Register extends User{
+        private $errors = [];
+        private $valid;
+
         public function __construct($login, $password, $email, $name)
         {
             if(validate::general($login, 'login') && validate::general($password, 'password') && validate::email($email) && validate::general($name, 'name')){
@@ -8,9 +11,27 @@
                 $this->setPasswordHash($this->generatePasswordHash($password));
                 $this->setEmail($email);
                 $this->setFullName($name);
+                $this->valid = true;
+            }
+            else
+            {
+                $this->valid = false;
+                array_push($this->errors, "Algum dos campos não foi preenchido corretamente.");
             }
             
         }
+
+        public function registrar(){
+            if(!$this->valid){
+                return false;
+            }
+
+        }
+
+        public function isLoginInUse(){
+
+        }
+
     }
 
 ?>
