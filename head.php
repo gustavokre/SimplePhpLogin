0c2a7ca9c603015911f0057a169ebe1fbb883d7d
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-03 03:34:43 -0300

    commit inicial

---
+++ head.php
@@ -0,0 +1,6 @@
+    <meta charset="UTF-8">
+	<meta name="author" content="Mário Gustavo Kreibich">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&family=Open+Sans&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="fontes.css">
+    
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tela de Login</title>
+<?php include "head.php";?>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="container">
+        <div class="container-login">
+            <p class="font-o font-size3">Login</p>
+            <form id="formLogin" action="login.php" method="POST">
+            <input type="text" name="login" value="" placeholder="Login"><br>
+            <input type="password" name="password" value="" placeholder="Password">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
