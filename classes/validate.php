32e22d8c64727425a2bf384f5bf5dfe5e19463f5
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-04 03:25:11 -0300

    new validate email

--- classes/validate.php
+++ classes/validate.php
@@ -38,13 +38,12 @@ class validate{
 		return false;
     }
     
-    public static function email($email){
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return true;
-        }
-        return false;
-    }
-
+		public static function email($email){
+			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				return true;
+			}
+			return false;
+		}
 }
 
 ?>
