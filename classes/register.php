d3c3179087b18eb1bb34093f75d74d47139b2d90
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-05 22:49:21 -0300

    Função de registrar novas contas adicionado

--- classes/register.php
+++ classes/register.php
@@ -21,15 +21,37 @@
             
         }
 
-        public function register(){
-            if(!$this->valid){
+        public function register(PDO $pdo){
+            if(!$this->valid || $this->isLoginAvailable($pdo)){
                 return false;
             }
-
+            $table = DatabaseConection::TABLENAME;
+            $stmt = $pdo->prepare("INSERT INTO $table (userLogin, password_hash, email, fullName) VALUES(:userLogin,:passwordHash,:email,:fullName)");
+			$stmt->bindValue(':userLogin', $this->getLogin(), PDO::PARAM_STR);
+            $stmt->bindValue(':passwordHash', $this->getPasswordHash(), PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->getEmail(), PDO::PARAM_STR);
+            $stmt->bindValue(':fullName', $this->getFullName(), PDO::PARAM_STR);
+            if(!$stmt->execute()) {
+                array_push($this->errors, $stmt->errorInfo());
+                return false;
+            }
+            return true;
         }
 
-        public function isLoginInUse(){
-            
+        public function isLoginAvailable(PDO $pdo){
+            $table = DatabaseConection::TABLENAME;
+            $stmt = $pdo->prepare("SELECT userLogin FROM $table WHERE userLogin=:userLogin");
+			$stmt->bindValue(':userLogin', $this->getLogin(), PDO::PARAM_STR);
+            $stmt->bindValue(':passwordHash', $this->getPasswordHash(), PDO::PARAM_STR);
+            if(!$stmt->execute()) {
+                array_push($this->errors, $stmt->errorInfo());
+                return false;
+            }
+            if($stmt->rowCount() > 0){
+                array_push($this->errors, "Usuário já registrado.");
+                return false;
+            }
+            return true;
         }
 
     }
