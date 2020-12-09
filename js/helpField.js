6eeffc64b484e1b4deddecfffa972625b77d73c8
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-09 05:07:21 -0300

    nome das funções foram trocadas, agora elas fazem mais sentido.

--- js/helpField.js
+++ js/helpField.js
@@ -2,16 +2,16 @@ let userFields = document.getElementsByClassName("userLogin");
 let helpFields = document.getElementsByClassName("helpField");
 let ActivedField = -1;
 for(let i = 0;i < userFields.length;i++){
-    userFields[i].addEventListener("focus", function(){ ShowHelp(i); }, false);
-    userFields[i].addEventListener("blur", function(){ hideFieldElements(); }, false);
+    userFields[i].addEventListener("focus", function(){ showHelpField(i); }, false);
+    userFields[i].addEventListener("blur", function(){ hideHelpField(); }, false);
 }
 
-function ShowHelp(id){
+function showHelpField(id){
     helpFields[id].classList.remove("invisible");
     ActivedField = id;
 }
 
-function hideFieldElements(){
+function hideHelpField(){
     if(ActivedField > -1){
         helpFields[ActivedField].classList.add("invisible");
     }
