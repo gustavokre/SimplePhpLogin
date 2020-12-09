cf621d2a5b4b3b915b22c7a21d73cd42b9217c76
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-09 03:26:08 -0300

    Implantado sistema multilinguagem, adicionado ajuda ao preencher campos

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once("classes/multilang.php");
+    require_once("classes/validate.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,14 +12,17 @@
 </head>
 <body>
     <div class="container">
-        <div class="container-login">
-            <p class="font-o font-size3">Login</p>
+        <div class="container-login" id="c-login">
+            <p class="font-o font-size3 text-center">Login</p>
             <form id="formLogin" action="logon.php" method="POST">
                 <input class="userLogin" type="text" name="login" value="" placeholder="Login"><br>
+                <div class="helpField font-t font-size1 invisible"><?php echo sprintf(MultiLang::getText("INDEX_LOGIN_HELP_FIELD"), Validate::INPUTMINSIZE['login'], Validate::INPUTMAXSIZE['login']);?></div>
                 <input class="userLogin" type="password" name="password" value="" placeholder="Password"><br>
-                <input class="font-size2 font-cor1" type="submit" name="btnLogin" value="Fazer login">
+                <div class="helpField font-t font-size1 invisible"><?php echo sprintf(MultiLang::getText("INDEX_PASSWORD_HELP_FIELD"), Validate::INPUTMINSIZE['password']);?></div>
+                <input class="font-size2 font-cor1" type="submit" name="btnLogin" value=<?php echo MultiLang::getText("INDEX_LOGIN_BUTTON");?>>
             </form>
         </div>
     </div>
+    <script src="js/helpField.js"></script>
 </body>
 </html>
