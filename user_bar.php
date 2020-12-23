8b68ebd47f6a2f955897fab6033a1d87f22af173
Mário Gustavo Kreibich <gukre92@gmail.com>
2020-12-23 13:52:33 -0300

    Adicionado botao de deslogar

--- user_bar.php
+++ user_bar.php
@@ -3,14 +3,15 @@
     const SESSION_DEBUG = false;
     if($userRR->go_online()){
         echo "<p style='color:#3a3;'>ONLINE!</p>";
+        echo "<span>" . MultiLang::get_text("SHOW_USER_NAME") . ": <b>" . $userRR->get_login() . "</b></span><br>";
+        echo "<span>" . MultiLang::get_text("SHOW_FULL_NAME") . ": <b>" . $userRR->get_full_name() . "</b></span><br>";
+        echo "<span>" . MultiLang::get_text("SHOW_EMAIL") . ": <b>" . $userRR->get_email() . "</b></span><br>";
+        echo "<a href='account/logout.php'>". MultiLang::get_text("LOGOUT_BUTTON") ."</a>";
     }
     else
     {
         echo "<p style='color:#a33;'>OFFLINE!</p>";
     }
-    echo "<span>" . MultiLang::get_text("SHOW_USER_NAME") . ": <b>" . $userRR->get_login() . "</b></span><br>";
-    echo "<span>" . MultiLang::get_text("SHOW_FULL_NAME") . ": <b>" . $userRR->get_full_name() . "</b></span><br>";
-    echo "<span>" . MultiLang::get_text("SHOW_EMAIL") . ": <b>" . $userRR->get_email() . "</b></span><br>";
     if(SESSION_DEBUG){
         echo "SESSION:";
         echo session_id() . "<br>";
